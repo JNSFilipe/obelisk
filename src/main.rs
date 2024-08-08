50f4885b8ce192f714857683bc95a405376f6add
JNSFilipe <jose.filipe@ieee.org>
2024-08-08 17:36:51 +0100

    Wacky... Thinking about rewriting in Go

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod obelisk;
 use obelisk::{LogLevel, create_symlink};
 
 fn main() {
+    create_symlink("/home/jfilipe/Arduino", "/tmp/Publi");
+
     log!(LogLevel::Info, "cenas {} {}" ,1,2);
     log!(LogLevel::Warning, "cenas {} {}" ,1,2);
     log!(LogLevel::Error, "cenas {} {}" ,1,2);
--- src/obelisk.rs
+++ src/obelisk.rs
@@ -1,4 +1,5 @@
 use std::env::consts::OS;
+use resolve_path::PathResolveExt;
 use std::fs::metadata;
 
 #[derive(Debug)]
@@ -23,3 +24,47 @@ macro_rules! log {
         println!("{} {}", tag, format!($fmt $(, $arg)*));
     }};
 }
+
+
+pub fn create_symlink(orig: &str, targ: &str) {
+    // TODO: Resolve relative paths
+    let res_orig = match orig.resolve().to_str() {
+        Some(s) => s,
+        None => panic!("I AM PANNICKING!!"),
+    };
+
+    let res_targ = match targ.resolve().to_str() {
+        Some(s) => s,
+        None => panic!("I AM PANNICKING!!"),
+    };
+
+    // For linux and macOS
+    #[cfg (any (target_os = "linux", target_os = "macos"))] {
+        match std::os::unix::fs::symlink(res_orig, targ) {
+            Ok(_) => {},
+            Err(e) => {
+                log!(LogLevel::Fatal, "Unnable to create symlink from {} to {} in {}", orig, targ, OS);
+            }
+        }
+
+    }
+
+    // For Windows
+    #[cfg (target_os = "windows")] {
+        // TODO: make error message in this unwarp
+        let md = metadata(orig).unwrap();
+
+        if md.is_dir() {
+            std::os::windows::fs::symlink_dir(orig, targ);
+        } else {
+            std::os::windows::fs::symlink_file(orig, targ);
+        }
+    }
+
+    // Anything else throws error
+    #[cfg (not (any (target_os = "windows", target_os = "linux", target_os = "macos")))]{
+        panic!("[ERR] Unsupported OS: {}!", OS);
+    }
+}
+
+
