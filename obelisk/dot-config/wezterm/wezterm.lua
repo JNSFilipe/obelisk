9ad6d408175b0dc123f43d5f8e96186f4e9cc93b
JNSFilipe <jose.filipe@ieee.org>
2024-12-11 00:02:45 +0000

    Updated a bunch of stuff, can't remember what exactly

--- obelisk/dot-config/wezterm/wezterm.lua
+++ obelisk/dot-config/wezterm/wezterm.lua
@@ -25,6 +25,11 @@ config.enable_wayland = false
 -- Hide titlebar
 config.window_decorations = "RESIZE"
 
+-- Make both option keys work on mac
+-- https://github.com/wez/wezterm/issues/5468#issuecomment-2133721206
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = true
+
 -- Blazingly fast!
 -- https://www.reddit.com/r/neovim/comments/1gthknw/wezterm_max_fps_240_is_crazy/
 config.max_fps = 240
