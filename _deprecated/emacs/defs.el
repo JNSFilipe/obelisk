86874792a3977dc29fc62564a8d8ce3a54783d22
JNSFilipe <jose.filipe@ieee.org>
2026-04-16 17:44:25 +0100

    chore: remove deprecated dotfiles
    Delete obsolete configuration files under _deprecated: bash/bashrc, bootstrap.sh, emacs/* (config.el, defs.el, init.el, keybinds.el, mono.el, packages.el), home.nix, semacs/spacemacs.el, and tmux/tmux.conf. Cleanup to remove legacy/unused dotfiles and reduce repository clutter; no functional changes aside from removing these deprecated assets.

--- _deprecated/emacs/defs.el
+++
@@ -1,4 +0,0 @@
-;;; ../../Documents/GitHub/monolith.nvim/demacs/defs.el -*- lexical-binding: t; -*-
-
-;; Keybinds! Enable custom keybinds and disable Doom's default ones
-(defvar custom-shortcuts t)
